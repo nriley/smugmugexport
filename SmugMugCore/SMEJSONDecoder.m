2c48379586752f4ae5537684d1b26f1d322991d6
Nicholas Riley <com-github@sabi.net>
2014-01-14 09:41:22 -0500

    Comment out unused variable.

--- SmugMugCore/SMEJSONDecoder.m
+++ SmugMugCore/SMEJSONDecoder.m
@@ -16,7 +16,7 @@
 }
 
 -(NSDictionary *)decodedResponse:(NSData *)smResponse {
-	NSString *responseString = [[[NSString alloc] initWithData:smResponse encoding:NSUTF8StringEncoding] autorelease];
+	// NSString *responseString = [[[NSString alloc] initWithData:smResponse encoding:NSUTF8StringEncoding] autorelease];
     return [[JSONDecoder decoder] objectWithData:smResponse];
 }
 
